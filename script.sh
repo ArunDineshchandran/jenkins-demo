99adc80bb498d6ca538ae959cafeb55482efe573
Arun dinesh <126953533+ArunDineshchandran@users.noreply.github.com>
2024-02-24 15:54:34 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,5 @@
 
 # This is a simple script
 echo "Hello world"
-echo "Adding the New one in Jenkins"
-echo "File has been added using the Hookup and it is Displaying in the Jenkins"
+echo "Adding new file in jenkins"
 
